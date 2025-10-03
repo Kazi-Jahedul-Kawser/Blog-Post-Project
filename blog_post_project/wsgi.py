182aecc6a02bce03f5da4124f3a85474072f7239
unknown <jkrafi149@gmail.com>
2025-10-04 01:18:55 +0600

    upgrade code

--- blog_post_project/wsgi.py
+++ blog_post_project/wsgi.py
@@ -1,17 +1,7 @@
-"""
-WSGI config for BLOG_PROJECT project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Replace 'BLOG_PROJECT.settings' with your actual project folder name
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog_post_project.settings")
-
 
 application = get_wsgi_application()
